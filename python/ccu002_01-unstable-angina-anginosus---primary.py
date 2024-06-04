e4f9636f8752273e61f2724afb887263f98ac654
Martin Chapman <contact@martinchapman.co.uk>
2024-06-04 10:37:08 +0100

    Update made by Phenoflow

---
+++ python/ccu002_01-unstable-angina-anginosus---primary.py
@@ -0,0 +1,25 @@
+# Rochelle Knight, Venexia Walker, et al., 2024.
+
+import sys, csv, re
+
+codes = [{"code":"19057007","system":"snomedct"},{"code":"G33z0","system":"snomedct"},{"code":"G311z","system":"snomedct"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('ccu002_01-unstable-angina-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["ccu002_01-unstable-angina-anginosus---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["ccu002_01-unstable-angina-anginosus---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["ccu002_01-unstable-angina-anginosus---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
